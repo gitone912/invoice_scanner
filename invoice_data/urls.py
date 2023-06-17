5f2ce1e2cc576c29108600a722940d4798602540
gitone912 <akash.101@kvsroranchi.in>
2023-06-18 00:25:08 +0530

    urls

--- invoice_data/urls.py
+++ invoice_data/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload/', views.upload_invoice, name='upload_invoice'),
-    path('', views.index, name='index'),
+    path('', views.upload_invoice, name='upload_invoice'),
+    # path('', views.index, name='index'),
 ]
